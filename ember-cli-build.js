be7f78a778f531015897c857e43d00c8b3326ffe
ashokaditya <ashokaditya.design@gmail.com>
2017-05-10 23:00:55 +0200

    excluding momentjs from ember build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,6 +18,6 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/semantic-ui/dist/semantic.min.js')
+  // app.import('bower_components/semantic-ui/dist/semantic.min.js')
   return app.toTree()
 }
