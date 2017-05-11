ad7cc920616a635174d939ce8b76d58ee25e58e8
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2017-05-11 15:43:23 +0200

    Trying ember-e3

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,10 +1,28 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
+  // tagName: 'e3',
   model () {
-    var url = 'http://10.33.0.97:4242/api/series/qf-top-10-in-bps'
-    return Ember.$.getJSON(url).then((response) => {
-      return response.data
-    })
+    // var url = 'http://10.33.0.97:4242/api/series/qf-top-10-in-bps'
+    // return Ember.$.getJSON(url).then((response) => {
+    //   return response.data
+    // })
+    return [
+      {
+        year: 2010,
+        rainfall: 12,
+        temperature: 86
+      },
+      {
+        year: 2011,
+        rainfall: 15,
+        temperature: 88
+      },
+      {
+        year: 2012,
+        rainfall: 21,
+        temperature: 90
+      }
+    ]
   }
 })
