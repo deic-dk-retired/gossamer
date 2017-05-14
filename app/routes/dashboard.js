8f69c74fbac5ac64fe22c89b22aa9140142d61d0
ashokaditya <ashokaditya.design@gmail.com>
2017-05-14 12:21:46 +0200

    nothing major here

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,11 +1,11 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-  // tagName: 'e3',
   model () {
     var url = 'http://10.33.0.97:4242/api/series/qf-top-10-in-bps'
-    return Ember.$.getJSON(url).then((response) => {
-      return response.data
-    })
+    return Ember.$.getJSON(url)
+      .then((response) => {
+        return response.data
+      })
   }
 })
