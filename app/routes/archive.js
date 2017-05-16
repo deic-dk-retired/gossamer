94b08ebb3b356e5cc0883f11df7c96c39285e6b2
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2017-05-16 14:48:21 +0200

    Changed dashboard model response object so that can render dashboard content based on the results

--- app/routes/archive.js
+++ app/routes/archive.js
@@ -3,9 +3,10 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   model () {
     var url = 'http://10.33.0.97:4242/api/rules'
-    return Ember.$.getJSON(url).then((response) => {
-      return response.data
-    })
+    return Ember.$.getJSON(url)
+      .then((response) => {
+        return response.data
+      })
   },
   actions: {
     showOlder () {
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
     var url = 'http://10.33.0.97:4242/api/series/qf-top-10-in-bps'
     return Ember.$.getJSON(url)
       .then((response) => {
-        return response.data
+        return response
       })
   }
 })
