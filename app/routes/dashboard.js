d845f88385769b9ea0dcde21c47abe6f4354a7c2
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2017-05-24 14:40:28 +0200

    Change of ip

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -3,7 +3,7 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   model () {
     // return this.store.findAll('series/qf-top-10-in-bps')
-    var url = 'http://10.33.0.97:4242/api/series/qf-top-10-in-bps'
+    var url = 'http://10.33.1.97:4242/api/series/qf-top-10-in-bps'
     return Ember.$.getJSON(url)
       .then((response) => {
         return response
--- app/routes/plan.js
+++ app/routes/plan.js
@@ -3,7 +3,7 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   model () {
     // return this.store.findAll('icmpt')
-    var url = 'http://10.33.0.97:4242/api/icmpt'
+    var url = 'http://10.33.1.97:4242/api/icmpt'
     return Ember.$.getJSON(url)
     .then((response) => {
       return response.data
