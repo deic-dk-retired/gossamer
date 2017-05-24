fce19bd6508ba8992f8524d13db656a4a17ae168
Ashokaditya <ashokaditya.design@gmail.com>
2017-05-24 08:06:59 +0200

    reworked json to conform to JSONAPIAdapter

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data'
 
 export default DS.Model.extend({
-  custId: DS.attr(),
+  custid: DS.attr(),
   accessType: DS.attr(),
   name: DS.attr(),
   username: DS.attr()
--- app/routes/rules.js
+++ app/routes/rules.js
@@ -2,11 +2,6 @@ import Ember from 'ember'
 
 export default Ember.Route.extend({
   model () {
-    // var url = 'http://10.33.0.97:4242/api/rules'
-    // return Ember.$.getJSON(url)
-    //   .then((response) => {
-    //     return response.data
-    //   })
     return this.store.findAll('rule')
   },
   actions: {
