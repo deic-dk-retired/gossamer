01851e0be974230369d7dbaeb9f18b1d549027ae
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2017-05-29 15:03:39 +0200

    Minor typos for model/user

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,7 @@ import DS from 'ember-data'
 
 export default DS.Model.extend({
   custid: DS.attr('number'),
-  accessType: DS.attr('string'),
+  accesstype: DS.attr('string'),
   name: DS.attr('string'),
   username: DS.attr('string')
 })
--- app/routes/users.js
+++ app/routes/users.js
@@ -2,9 +2,6 @@ import Ember from 'ember'
 
 export default Ember.Route.extend({
   model () {
-    return this.store.findAll('user')
-  },
-  afterModel () {
-    return this.store.peekAll('user')
+    return this.get('store').findAll('user')
   }
 })
