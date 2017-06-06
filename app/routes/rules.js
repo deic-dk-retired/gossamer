06cc0def1fe3d963fdc41c42bb9ad5411bba2024
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2017-06-06 15:47:31 +0200

    Enabled logging, added loading animation, small corrections to ui

--- app/routes/rules.js
+++ app/routes/rules.js
@@ -2,10 +2,20 @@ import Ember from 'ember'
 
 export default Ember.Route.extend({
   model () {
-    return this.get('store').findAll('rule')
+    return this.get('store').findAll('rule').then((results) => {
+      return {
+        rules: results,
+        meta: results.get('meta')
+      }
+    })
+  },
+  setupController (controller, { rules, meta }) {
+    this._super(controller, rules)
+    controller.set('meta', meta)
   },
   actions: {
-    showOlder () {
+    showMore () {
+      console.log('more..')
     }
   }
 })
