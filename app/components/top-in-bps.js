463cfcf2b590774f4c755c69a22f6f7d65aaa7cf
Ashokaditya <ashokaditya.design@gmail.com>
2017-06-07 13:36:31 +0200

    charts with static data from influx db

--- app/components/top-in-bps.js
+++ app/components/top-in-bps.js
@@ -3,15 +3,66 @@ import { select } from 'd3-selection'
 // import data from 'dashboard'
 
 export default Ember.Component.extend({
+  // tagName: '',
+  data: [
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 768,
+      'cidr': '130_226_136_243'
+    },
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 712,
+      'cidr': '130_226_136_248'
+    },
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 688,
+      'cidr': '130_226_136_242'
+    },
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 672,
+      'cidr': '130_226_136_244'
+    },
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 664,
+      'cidr': '130_226_136_251'
+    },
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 664,
+      'cidr': '130_226_136_240'
+    },
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 664,
+      'cidr': '130_226_136_245'
+    },
+    {
+      'x': '2017-06-07T10:01:12Z',
+      'y': 664,
+      'cidr': '130_226_136_252'
+    },
+    {
+      'x': '2017-06-07T10:14:10Z',
+      'y': 728,
+      'cidr': '130_226_136_252'
+    },
+    {
+      'x': '2017-06-07T10:14:10Z',
+      'y': 680,
+      'cidr': '130_226_136_255'
+    }
+  ],
   didInsertElement () {
     let svg = select(this.$('svg')[0])
-
-    svg.selectAll('rect')
-    .data(data)
+    svg.selectAll('rect').data(this.get('data'))
     .enter()
     .append('rect')
     .attr('width', 20)
-    .attr('height', d => d.x)
-    .attr('x', (d, i) => i * 25)
+    .attr('height', data => (data.y / 3))
+    .attr('x', (data, i) => i * 22)
   }
 })
