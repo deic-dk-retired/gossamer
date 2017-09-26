1c6f8659b8f56d0fb7d4371decc19bbd5102a1bb
Ashokaditya <ashokaditya.design@gmail.com>
2017-09-26 11:14:54 +0200

    rules controller

--- app/routes/rules.js
+++ app/routes/rules.js
@@ -3,13 +3,14 @@ import Ember from 'ember'
 export default Ember.Route.extend({
   resultPromise: {},
   model () {
-    return this.get('store').findAll('rule')
+    return Ember.RSVP.hash({
+      rules: this.get('store').findAll('rule')
+    })
   },
-  afterModel () {
-  },
-  actions: {
-    showMore () {
-      console.log('more..')
-    }
+
+  setupController (controller, model) {
+    this._super(...arguments)
+
+    Ember.set(controller, 'rules', model.rules)
   }
 })
