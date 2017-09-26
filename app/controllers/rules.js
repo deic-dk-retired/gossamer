a02201d2292a812a38b01c94491deab57180e65b
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2017-09-26 15:29:53 +0200

    rules controller to show rule detail on click; segments for rules, users and customer edit forms;

--- app/controllers/rules.js
+++ app/controllers/rules.js
@@ -1,5 +1,57 @@
 import Ember from 'ember'
 
 export default Ember.Controller.extend({
+  rid: null,
+  fnm: null,
+  coid: null,
+  rname: null,
+  uid: null,
+  way: '',
+  active: null,
+  expired: null,
+  proto: '',
+  action: '',
 
+  init () {
+    this._super(...arguments)
+    this.errors = []
+    this.act = 'Save'
+    this.buttonico = 'save'
+  },
+
+  actions: {
+    required (event) {
+      if (!event.target.value) {
+        this.get('errors').pushObject({message: `${event.target.name} is required`})
+      }
+    },
+
+    toggleActive (set, toSet) {
+      if (set !== toSet) {
+        Ember.$('.card').removeClass('active')
+        Ember.$('.rule-' + toSet).addClass('active')
+        Ember.$('.togDisabled').removeClass('disabled')
+        // Ember.$('.segment').removeClass('hidden')
+      }
+    },
+
+    showRule (rid) {
+      Ember.Logger.info(rid)
+      let rule = this.get('store').peekRecord('rule', rid)
+      this.send('toggleActive', this.get('rid'), rid)
+      this.setProperties({
+        rid: parseInt(rid),
+        fnm: rule.get('fnmid'),
+        coid: rule.get('custid'),
+        rname: rule.get('rname'),
+        uid: rule.get('adminid'),
+        way: rule.get('direct'),
+        active: rule.get('isactive'),
+        expired: rule.get('isexpired'),
+        proto: rule.get('ipprotocol'),
+        action: rule.get('action')
+      })
+    }
+
+  }
 })
---
+++ tests/integration/components/rule-edit-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('rule-edit', 'Integration | Component | rule edit', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{rule-edit}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#rule-edit}}
+      template block text
+    {{/rule-edit}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
