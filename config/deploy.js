0e2935b0e4f9e0de68436220c7ff91423662d24a
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2018-03-22 13:27:15 +0100

    ember-cli deploy config file and codeclimate badge

---
+++ config/deploy.js
@@ -0,0 +1,48 @@
+/* eslint-env node */
+'use strict'
+
+module.exports = function (deployTarget) {
+  let ENV = {
+    build: {},
+    APP: {
+      PORT: 8686,
+      SCPSS: process.env.SU_SEC_3SHA512,
+      SERV_HOST: process.env.SERV_HOST,
+      SERV_PORT: process.env.SERV_HOST,
+      SERV_API: process.env.SERV_NAMESPACE
+    },
+    'ember-cli-notifications': {
+      includeFontAwesome: true
+    },
+    'ember-simple-auth': {
+      authorzer: 'authorizer:custom',
+      routeAfterAuthentication: '/',
+      routeIfAlreadyAuthenticated: '/dashboard'
+    }
+    // include other plugin configuration that applies to all deploy targets here
+  }
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development'
+    ENV.APP.SERV_PORT = 9696
+    ENV.APP.PORT = 8686
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'staging'
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production'
+    ENV.APP.SERV_PORT = 9090
+    ENV.APP.PORT = 8080
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV
+}
