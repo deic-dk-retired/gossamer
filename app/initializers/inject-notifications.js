ac651e72df30e410aa7c0c09c6698a7315ca798b
ashokaditya <ashokaditya.design@gmail.com>
2018-04-18 13:48:58 +0200

    initialize notifications application wide #20

--- app/initializers/inject-notifications.js
+++ app/initializers/inject-notifications.js
@@ -1,5 +1,6 @@
 export function initialize (application) {
   application.inject('controller', 'notifications', 'service:notification-messages')
+  application.inject('route', 'notifications', 'service:notification-messages')
 }
 
 export default {
