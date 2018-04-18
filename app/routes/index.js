69ecdc679c4c984ce357d191b21735f438651516
Ashokaditya Mohanty <ashokaditya.design@gmail.com>
2018-04-18 13:32:56 +0200

    app should log out on token expiration #13

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,18 +4,40 @@ import ms from 'npm:ms'
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service('session'),
+  notifications: Ember.inject.service('notification-messages'),
+
   timeToExp: Ember.computed('session', function () {
-    return `${ms(this.get('session.data.authenticated.texp'))}`
+    let session = this.get('session')
+    let texp = 0
+    if (session.authenticator !== null && !Ember.isEmpty(session) && session.isAuthenticated) {
+      texp = ms(this.get('session.data.authenticated.texp'))
+    }
+    return texp
   }),
+
+  min: ms('1m'),
 
   beforeModel () {
     this._super(...arguments)
     let session = this.get('session')
+    // let texp = 0
     if (!Ember.isEmpty(session) && session.isAuthenticated) {
+      Ember.run.next(this, () => {
+        this.get('notifications').info(`Your session will expire in ${ms(this.get('timeToExp'), { long: true })}`, {
+          autoClear: true,
+          clearDuration: 5000
+        })
+      })
       Ember.run.later(this, () => {
-        Ember.Logger.info(this.get('timeToExp'))
         session.invalidate()
       }, this.get('timeToExp')).bind(this)
+
+      Ember.run.later(this, () => {
+        this.get('notifications').warning(`Your session is about to expire in ${ms(this.get('min'), { long: true })}`, {
+          autoClear: true,
+          clearDuration: this.get('min')
+        })
+      }, this.get('timeToExp') - this.get('min')).bind(this)
       this.transitionTo('dashboard')
     }
 
