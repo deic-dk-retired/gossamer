66ccf70fd4fab48a5f9a4869ed680bab445e1f48
ashokaditya <ashokaditya.design@gmail.com>
2018-06-28 12:32:51 +0200

    fixes all but one for #45

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,19 +1,28 @@
 import Ember from 'ember'
 
 export default Ember.Controller.extend({
-  loginFailed: false,
-  processed: false,
+  loginOk: false,
+  processed: '',
   errorMessage: null,
+  showPass: false,
+  passType: Ember.computed('showPass', function () {
+    let intype = 'password'
+    if (this.get('showPass')) {
+      intype = 'text'
+    }
+    return intype
+  }),
 
   actions: {
+
+    showPassword () {
+      this.toggleProperty('showPass')
+    },
+
     authenticate () {
       let { username, password } = this.getProperties('username', 'password')
+      this.set('loginOk', true)
       let session = this.get('session')
-      this.set('processed', `Verifying...`)
-      this.get('notifications').info(this.get('processed'), {
-        autoClear: true,
-        clearDuration: 500
-      })
       session.authenticate('authenticator:jwt', {username, password})
       .then(() => {
         this.set('processed', `Welcome ${this.get('session.data.authenticated.ualias').split(' ')[0]}`)
@@ -23,7 +32,7 @@ export default Ember.Controller.extend({
         })
       })
       .catch((err) => {
-        this.set('loginFailed', true)
+        this.set('loginOk', false)
         this.set('errorMessage', err.responseJSON.message || 'error')
         this.get('notifications').clearAll()
         this.get('notifications').error(this.get('errorMessage'), {
